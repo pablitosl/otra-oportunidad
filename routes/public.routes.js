0f67e3dc8841a0acbf8e8d1b4533f57a4f53a700
AaronMonti <montiveroaaron@gmail.com>
2022-11-30 13:16:21 -0300

    signup

--- routes/public.routes.js
+++ routes/public.routes.js
@@ -6,12 +6,29 @@ routerPublic.get('/', (req, res) => {
     res.render('home', {layout: 'landing'})
 })
 
-routerPublic.get('/all', async (req, res) => {
+/* routerPublic.get('/all', async (req, res) => {
+    const query = req.query.sort
+    console.log(query.sort)
 
     try {
+            const jobs = await jobModel.find({}).lean() // .lean() => Dejarme lo que devuelve como un objeto vanilla javascript
+        //Verifico que existan
+        res.status(200).render('index', { layout: 'main', jobs })
 
+
+    } catch (error) {
+        console.log(error)
+    }
+})
+ */
+routerPublic.get('/all', async (req, res) => {
+
+/*     const query = req.params
+    console.log(query) */
+    /* .sort(sort) */
+    var sort = { title: 1}
+    try {
         const jobs = await jobModel.find({}).lean() // .lean() => Dejarme lo que devuelve como un objeto vanilla javascript
-
         //Verifico que existan
 
         res.status(200).render('index', { layout: 'main', jobs })
